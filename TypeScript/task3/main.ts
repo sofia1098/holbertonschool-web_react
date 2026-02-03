80f0475fed3b409dcd34d1f03b203b959fbfd7af
sofia1098 <sofianunez000@gmail.com>
2026-02-03 00:37:31 +0000

    Add intefaces.ts

---
+++ TypeScript/task3/main.ts
@@ -0,0 +1,21 @@
+/// <reference path="./js/crud.d.ts" />
+
+import { RowID, RowElement } from './interface';
+import * as CRUD from './js/crud';
+
+const row: RowElement = {
+  firstName: 'Guillaume',
+  lastName: 'Salva',
+};
+
+const newRowID: RowID = CRUD.insertRow(row);
+console.log(`New row ID: ${newRowID}`);
+
+const updatedRow: RowElement = {
+  firstName: 'Guillaume',
+  lastName: 'Salva',
+  age: 23,
+};
+
+CRUD.updateRow(newRowID, updatedRow);
+CRUD.deleteRow(newRowID);
