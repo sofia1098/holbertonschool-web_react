d210973bb46154f7941532aa408b889fd6cb0bc7
sofia1098 <sofianunez000@gmail.com>
2026-02-03 00:30:33 +0000

    Add task3

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -11,4 +11,29 @@ interface Directors extends Teacher {
   numberOfReports: number;
 }
 
-function printTeacher(firstName: string, lastName: string): string { return `${firstName.charAt(0)}. ${lastName}`;}
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+interface StudentConstructor {
+  new(firstName: string, lastName: string): StudentClassInterface;
+}
+
+function printTeacher(firstName: string, lastName: string): string { return `${firstName.charAt(0)}. ${lastName}`; }
+
+class StudentClass implements StudentClassInterface {
+  private firstName: string;
+  private lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+  displayName(): string {
+    return this.firstName;
+  }
+}
